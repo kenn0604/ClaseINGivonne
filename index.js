f4674ca3d28248d6a21251b9c12a6491fb9e63bc
KENY <KENY@TELLO.com>
2025-05-16 19:49:48 -0600

    [ADD], se agrego la variable numero

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("----- TIPOS DE VARIABLES -----");
+
+console.log("Tipo number");
+// Número entero (dato cuantitativo)
+let numero;
+numero = 42;
+console.log("Este es el valor de numero:", numero);
+
